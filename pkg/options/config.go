991e321de1a716a82a20dcb57bcd57e525b9d6a8
Nikita Egorov <n.egorov@restream.rt.ru>
2019-11-23 12:36:13 +0300

    Чтение конфигурации

---
+++ pkg/options/config.go
@@ -0,0 +1,150 @@
+package options
+
+import (
+	"encoding/base64"
+	"net/http"
+	"time"
+)
+
+type config struct {
+	Core    *ConfigCore `yaml:"core"`
+	Host    *ConfigHost `yaml:"host"`
+	Scripts []*ConfigScript   `yaml:"scripts"`
+}
+
+// ConfigCore базовые настройки теста
+type ConfigCore struct {
+	Thread      int           `yaml:"threads"`
+	Connections int           `yaml:"threads"`
+	Duration    time.Duration `yaml:"duration"`
+}
+
+// Реализация десириализации коры
+func (c *ConfigCore) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var tc struct {
+		Thread      int    `yaml:"threads"`
+		Connections int    `yaml:"connections"`
+		Duration    string `yaml:"duration"`
+	}
+	var e error
+
+	if e = unmarshal(&tc); e != nil {
+		return e
+	}
+
+	if c.Duration, e = time.ParseDuration(tc.Duration); e != nil {
+		return e
+	}
+	c.Thread = tc.Thread
+	c.Connections = tc.Connections
+
+	return nil
+}
+
+type headerEntry struct {
+	Name  string `yaml:"name"`
+	Value string `yaml:"value"`
+}
+
+type ConfigHost struct {
+	Address  string      `yaml:"addr"`
+	Port     int         `yaml:"port"`
+	Protocol string      `yaml:"protocol"`
+	Header   http.Header `yaml:"header"`
+}
+
+func (c *ConfigHost) UnmarshalYaml(unmarshal func(interface{}) error) error {
+	var tc struct {
+		Address  string        `yaml:"addr"`
+		Port     int           `yaml:"port"`
+		Protocol string        `yaml:"protocol"`
+		Header   []headerEntry `yaml:"header,omitempty"`
+	}
+
+	if e := unmarshal(&tc); e != nil {
+		return e
+	}
+	c.Address = tc.Address
+	c.Port = tc.Port
+	c.Protocol = tc.Protocol
+
+	c.Header = make(http.Header)
+	for _, ent := range tc.Header {
+		c.Header.Add(ent.Name, ent.Value)
+	}
+	return nil
+}
+
+type ConfigScript struct {
+	Name     string          `yaml:"name"`
+	Header   http.Header     `yaml:"header,omitempty"`
+	Requests []ConfigRequest `yaml:"requests"`
+}
+
+func (c *ConfigScript) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var tc struct {
+		Name     string          `yaml:"name"`
+		Header   []headerEntry   `yaml:"header,omitempty"`
+		Requests []ConfigRequest `yaml:"requests"`
+	}
+
+	if e := unmarshal(&tc); e != nil {
+		return e
+	}
+	c.Name = tc.Name
+	c.Requests = tc.Requests
+	c.Header = make(http.Header)
+	for _, ent := range tc.Header {
+		c.Header.Add(ent.Name, ent.Value)
+	}
+	return nil
+}
+
+type ConfigRequest struct {
+	Resource string      `yaml:"resource"`
+	Method   string      `yaml:"method,omitempty"`
+	Body     []byte      `yaml:"body,omitempty"`
+	Rate     int         `yamle:"rate,omitempty"`
+	Header   http.Header `yaml:"header,omitempty"`
+}
+
+func (c *ConfigRequest) UnmarshalYAML(unmarshal func(interface{}) error) (e error) {
+	var tc struct {
+		Resource string          `yaml:"resource"`
+		Method   string          `yaml:"method,omitempty"`
+		Body     string          `yaml:"body,omitempty"`
+		Rate     int             `yamle:"rate,omitempty"`
+		Header   []headerEntry   `yaml:"header,omitempty"`
+	}
+	e = nil
+
+	if e = unmarshal(&tc); e != nil {
+		return
+	}
+
+	c.Resource = tc.Resource
+	c.Method = tc.Method
+	if c.Body, e = base64.StdEncoding.DecodeString(tc.Body); e != nil {
+		return
+	}
+	c.Rate = tc.Rate
+	c.Header = make(http.Header)
+	for _, ent := range tc.Header {
+		if c.Header == nil {
+			panic("Nil header WAT!")
+		}
+		c.Header.Set(ent.Name, ent.Value)
+	}
+	return
+}
+
+func MergeHttpHeaders(headers ...http.Header) http.Header {
+	h := http.Header{}
+
+	for _, header := range headers {
+		for k, _ := range header {
+			h.Set(k, header.Get(k))
+		}
+	}
+	return h
+}
---
+++ pkg/options/config_test.go
@@ -0,0 +1,157 @@
+package options
+
+import (
+	"bytes"
+	yaml "gopkg.in/yaml.v2"
+	"testing"
+	"net/http"
+)
+
+var headerConfig = []byte(`
+name: Content-Type
+value: text/json
+`)
+
+func TestHeaderEntry(t *testing.T) {
+	cfg := headerEntry{}
+
+	if err := yaml.Unmarshal(headerConfig, &cfg); err != nil {
+		t.Errorf("Can't unmarsha cfg: %s", err.Error())
+	}
+	if cfg.Name != "Content-Type" {
+		t.Errorf("Wrong Name value: '%s' instead Content-Type", cfg.Name)
+	}
+	if cfg.Value != "text/json" {
+		t.Errorf("Wrong Value value: '%s' instead text/json", cfg.Value)
+	}
+}
+
+var requestConfig = []byte(`
+resource: /hls/CH_UFCHD_HLS/master.m3u8
+method: POST
+body:   dXNlcjpwYXNzd29yZA==
+rate:   42
+header:
+    - name: X-TYPE
+      value: Test
+    - name: X-SIZE
+      value: empty
+`)
+
+func TestConfigRequest(t *testing.T) {
+	cfg := ConfigRequest{}
+
+	if err := yaml.Unmarshal(requestConfig, &cfg); err != nil {
+		t.Errorf("Can't unmarsha cfg: %s", err.Error())
+	}
+
+	if cfg.Resource != "/hls/CH_UFCHD_HLS/master.m3u8" {
+		t.Errorf("Wrong resource '%s'", cfg.Resource)
+	}
+	if cfg.Method != "POST" {
+		t.Errorf("Wrong method '%s'", cfg.Method)
+	}
+	if bytes.Equal(cfg.Body, []byte(`user:password\0`)) {
+		t.Errorf("Wrong body %v", string(cfg.Body))
+	}
+	if cfg.Rate != 42 {
+		t.Errorf("Wrong rate")
+	}
+	if cfg.Header.Get("X-TYPE") != "Test" || cfg.Header.Get("X-SIZE") != "empty" {
+		t.Errorf("Wrong header")
+	}
+}
+
+var scriptConfig = []byte(`
+name: 631e8c62-419e-46a2-b6cc-c3862c150843
+header:
+      - name: Content-Type
+        value: text/m3u8
+      - name: X-User-Token
+        value: e04ce87c-74a9-4431-a829-4635f53583f3
+requests:
+      - resource: /enter
+        method: POST
+        body: VVNFUjpQQVNTV09SRA==
+      - resource: /get/master.m3u8
+        header:
+          - name: Content-Type
+            value: text/mediaplaylist
+          - name: X-User-Token
+            value: e04ce87c-74a9-4431-a829-4635f53583f3
+      - resource: /get/media.m3u8
+        rate: 100`)
+
+func TestConfigScript(t *testing.T) {
+	cfg := ConfigScript{}
+
+	if e := yaml.Unmarshal(scriptConfig, &cfg); e != nil {
+		t.Errorf("Failed to unmarshal: %s", e.Error())
+	}
+
+	if cfg.Name != "631e8c62-419e-46a2-b6cc-c3862c150843" {
+		t.Errorf("Wromg script name")
+	}
+
+	if cfg.Header.Get("Content-Type") != "text/m3u8" ||
+		cfg.Header.Get("X-User-Token") != "e04ce87c-74a9-4431-a829-4635f53583f3" {
+		t.Errorf("Wrong headers")
+	}
+
+	if len(cfg.Requests) != 3 {
+		t.Errorf("Wrong number of requests")
+	}
+}
+
+var hostConfig = []byte(`
+addr: 127.0.0.1
+port: 8090
+protocol: http`)
+
+func TestConfigHost(t *testing.T) {
+	cfg := ConfigHost{}
+
+	if e := yaml.Unmarshal(hostConfig, &cfg); e != nil {
+		t.Errorf("Failed to unmarshal %s", e.Error())
+	}
+
+	if cfg.Address != "127.0.0.1" {
+		t.Errorf("Wrong host in config")
+	}
+	if cfg.Port != 8090 {
+		t.Errorf("Wrong port number")
+	}
+	if cfg.Protocol != "http" {
+		t.Errorf("Wrong prtocol")
+	}
+}
+
+// TODO: Здесь должен быть тест на проверку конфигурации ядра. Но пока
+// не понятно что еще я добалю в параметры, поэтому не буду его
+// добавлять
+
+func TestMergeHttpHedaers(t *testing.T) {
+	alpha := make(http.Header)
+	betta := make(http.Header)
+	gamma := make(http.Header)
+
+	alpha.Set("Content-Type", "bytes/octetstream")
+	alpha.Set("Allow", "GET,OPTIONS,HEAD")
+	alpha.Set("X-BEST-BEFORE", "today")
+
+	betta.Set("Content-Type", "bytes/octetstream")
+	betta.Set("Allow", "POST")
+
+	gamma.Set("Content-Type", "text/json")
+
+	final := MergeHttpHeaders(alpha, betta, gamma)
+	if final.Get("Content-Type") != "text/json" {
+		t.Errorf("Wrong Content-Type: %s", final.Get("Content-Type"))
+	}
+	if final.Get("Allow") != "POST" {
+		t.Errorf("Wrong Allow: %s", final.Get("Allow"))
+	}
+	if final.Get("X-BEST-BEFORE") != "today" {
+		t.Errorf("Wrong X-BEST-BEFORE: %s", final.Get("X-BEST-BEFORE"))
+	}		
+}
